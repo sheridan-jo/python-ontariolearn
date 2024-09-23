f582233a40855fc9b38c595f0f5cfb542c9c12c5
John O <jsolenski@hotmail.com>
2024-09-23 07:55:39 -0400

    Update hello.py
    Added documentation explaining how to run the program.

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-#  Prints 'Hello, world!'
+"""
+The "Hello World" Program
+
+This is a program that displays "Hello, world!" It is used for practice in creating a repository.
+To use it, run `hello.py`.
+"""
+
 print('Hello, world!')
